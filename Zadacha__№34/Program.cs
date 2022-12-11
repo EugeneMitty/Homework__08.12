8e4256f24162240bca98132b300f2fbd900aaa29
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-11 13:27:01 +0300

    111

--- Zadacha__№34/Program.cs
+++ Zadacha__№34/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите массив");
+int size = Convert.ToInt32(Console.ReadLine);
+int[] numbers = new int[size];
+RandonNumbers(numbers);
+Console.WriteLine("Массив ");
+PrintArray(numbers);
+
+void RandonNumbers(int[] numbers)
+{
+    for (int i = 0; i<size; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+
